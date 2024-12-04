692b5d855dbff7f133b7730ef15ac4898f40c8c3
Tayyab Ali <63280240+tayybi@users.noreply.github.com>
2024-12-04 14:52:13 -0500

    Update main.java

--- main.java
+++ main.java
@@ -9,7 +9,7 @@ public class Main {
     while(n <= 10) {
       sum = sum + n;
 
-      if(args.length != 0 &&  /* TO COMPLETE */) {
+      if(args.length != 0 &&  args[0].equals("trace_on")) {
         System.out.println("n = " + n);
         System.out.println("sum = " + sum);
       }
@@ -19,5 +19,5 @@ public class Main {
     System.out.println("Sum = " + sum);
   }
 
-  
+
 }
